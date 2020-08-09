d03a2b06417bb71fc42a37a4435b094419e75ca1
Simon Chan <dev.simon.chan@gmail.com>
2020-08-09 16:35:33 -0500

    able to get country data (cases, recovered, deaths) displayed

--- src/App.js
+++ src/App.js
@@ -7,10 +7,21 @@ import {
   CardContent
 } from '@material-ui/core';
 import './App.css';
+import InfoBox from './components/InfoBox';
+import Map from './components/Map'
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      })
+  }, [])
 
   useEffect(() => {
     const getCountryData = async () => {
@@ -36,38 +47,54 @@ function App() {
 
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    console.log(event.target);
-    console.log(countryCode);
-    setCountry(countryCode);
+    
+    const url = countryCode === 'worldwide' 
+    ? 'https://disease.sh/v3/covid-19/all'
+    : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        console.log(countryCode);
+        setCountryInfo(data);
+        console.log(data);
+      })
   };
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID 19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            {/* loop through countries */}
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            { countries.map((country) => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID 19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+              {/* loop through countries */}
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              { countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronarvirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+
+        {/* Map */}
+        <Map />
       </div>
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live cases by country</h3>
 
-      {/* HEADER */}
-      {/* TITLE + Select drop down */}
-
-      {/* Infoboxes */}
-      {/* Infoboxes */}
-      {/* Infoboxes */}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/components/InfoBox.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+
+function InfoBox({ title, cases, total }) {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                <Typography color="textSecondary" className="infoBox__title">
+                    { title }
+                </Typography>
+                <h2 className="inforBox__cases">
+                    { cases }
+                </h2>
+                <Typography color="textSecondary" className="infoBox__total">
+                    { total }
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
